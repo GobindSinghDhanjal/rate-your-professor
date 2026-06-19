9a5f6ca633e07e5c2c8e91b2153dc3a50c9f9ae9
Gobind <gobindsinghdhanjal1@gmail.com>
2026-06-19 16:40:11 +0530

    updated the sitemap url

--- app/sitemap.js
+++ app/sitemap.js
@@ -6,10 +6,9 @@ export default async function sitemap() {
   let universities = [];
 
   try {
-    const resProfessors = await fetch(
-      `${process.env.NEXT_PUBLIC_NEXT_BASE_URL}/professors`,
-      { cache: "no-cache" },
-    );
+    const resProfessors = await fetch(`${process.env.BASE_URL}/professors`, {
+      cache: "no-cache",
+    });
     professors = await resProfessors.json();
   } catch (error) {
     console.error("Error fetching professors for sitemap:", error);
@@ -17,7 +16,7 @@ export default async function sitemap() {
 
   try {
     const resUniversities = await fetch(
-      `${process.env.NEXT_PUBLIC_NEXT_BASE_URL}/universities`,
+      `${process.env.BASE_URL}/universities`,
       { cache: "force-cache" },
     );
     universities = await resUniversities.json();
